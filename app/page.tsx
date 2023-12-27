de03b085d6ebca397db3b8b8f3455fdbf39cd116
Adam Trager <hello@adamtrager.com>
2023-12-26 21:14:50 -0500

    link to albion data project

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,21 @@ export default async function Home() {
 
   return (
     <Fragment>
-      Buy from sell orders, sell to buy orders. Tax is included in the sale
-      price.
+      <div className="mb-4">
+        Buy from sell orders, sell to buy orders. Tax is included in the sale
+        price.
+        <br />
+        Prices out of date? Help us out by contributing to the{" "}
+        <a
+          className="text-blue-600 dark:text-blue-500 hover:underline"
+          href="https://www.albion-online-data.com/"
+          target="_blank"
+          rel="noopener"
+        >
+          Albion Data Project
+        </a>
+        . It&apos;s fast and free!
+      </div>
       <Table marketData={marketData} />
     </Fragment>
   );
